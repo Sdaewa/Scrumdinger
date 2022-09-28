b1a6adb4a17b2f7551a717cac1f0390d0cd21a5b
Luis Ramirez <sdaewa22@gmail.com>
2022-09-28 19:41:09 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Luis R. on 28.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
